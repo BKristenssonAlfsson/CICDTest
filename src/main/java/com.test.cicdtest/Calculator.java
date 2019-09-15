2758e9cf2012a8fc1397762288fb9d81b0c85fe8
Björn <bjorn.alfsson@gmail.com>
2019-09-15 19:43:10 +0200

    A subtraction method and test added (Test will fail)

--- src/main/java/com.test.cicdtest/Calculator.java
+++ src/main/java/com.test.cicdtest/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public Integer add(int number1, int number2) {
         return number1 + number2;
     }
+
+    public Double subtract(double number1, double number2){
+        return number1 - number2;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -19,4 +19,9 @@ public class CalculatorTest {
 
         Assertions.assertEquals(4, calc.add(2,2), "2 + 2 should be 4");
     }
+
+    @Test
+    public void twoPointFiveMinusOnePointThreeEqualsOnePointTwo() {
+        Assertions.assertEquals(1.2, calc.subtract(2.5, 2), "2.5 - 1.3 should be 1.2" );
+    }
 }
